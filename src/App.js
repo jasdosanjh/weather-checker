d61713277f9e1ead0af82d5cc4f94912b9e494c9
jazdosanjh <jaspinder@live.co.uk>
2018-02-13 20:58:21 +0000

    Weather API
    Added the weather API to app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from "./components/Weather";
 
+const API_KEY = "4cdbc063596c890b8be4873d2d920a4c"
+
 class App extends React.Component {
+    getWeather = async () => {
+        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${API_KEY}&units=metric`);
+        const data = await api_call.json();
+        console.log(data);    
+    }
     render() {
         return (
             <div>
