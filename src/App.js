a29011c4d97b30fd45a7f6c7c05333128e88dec4
jazdosanjh <jaspinder@live.co.uk>
2018-02-14 19:44:37 +0000

    CSS
    Added styling to the application.

--- src/App.js
+++ src/App.js
@@ -37,26 +37,41 @@ class App extends React.Component {
                 country: undefined,
                 humidity: undefined,
                 description: undefined,
-                error: "Please enter the value."
+                error: "Please enter a location."
         });
     }
 }
     render() {
         return (
             <div>
-                <Titles />
-                <Form getWeather={this.getWeather}/>
-                <Weather 
-                    temperature={this.state.temperature}
-                    city={this.state.city} 
-                    country={this.state.country}
-                    humidity={this.state.humidity}
-                    description={this.state.description}
-                    error={this.state.error}
-                />
+                <div className="wrapper">
+                    <div className="main">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-xs-5 title-container">
+                                    <Titles />
+                                </div>
+                                <div className="col-xs-7 form-container">
+                                <Form getWeather={this.getWeather}/>
+                                <Weather 
+                                    temperature={this.state.temperature}
+                                    city={this.state.city} 
+                                    country={this.state.country}
+                                    humidity={this.state.humidity}
+                                    description={this.state.description}
+                                    error={this.state.error}
+                                />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
 };
+
+
+
 
 export default App;
